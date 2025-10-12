f770ee3362e55b65221a978a9dbddd438c714e66
eddieogola <edwineogola@gmail.com>
2025-10-13 02:00:41 +0300

    feat: remove obsolete test files and update product service to use gRPC

--- server/productservice/go.mod
+++ server/productservice/go.mod
@@ -2,12 +2,16 @@ module github.com/eddieogola/stickerm/server/productservice
 
 go 1.25.1
 
-require google.golang.org/protobuf v1.36.10
+require (
+	google.golang.org/grpc v1.76.0
+	google.golang.org/protobuf v1.36.10
+)
 
 require (
 	golang.org/x/net v0.46.0 // indirect
 	golang.org/x/sys v0.37.0 // indirect
 	golang.org/x/text v0.30.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20251007200510-49b9836ed3ff // indirect
-	google.golang.org/grpc v1.76.0 // indirect
 )
+
+replace github.com/eddieogola/stickerm/server/productservice/controllers => ./controllers
--- server/productservice/main.go
+++ server/productservice/main.go
@@ -1,11 +1,36 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+
+	controllers "github.com/eddieogola/stickerm/server/productservice/controllers"
+	pb "github.com/eddieogola/stickerm/server/productservice/genproto"
 )
 
-
+var (
+	port = flag.Int("port", 50051, "The server port")
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	flag.Parse()
+
+	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", *port))
+
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	var opts []grpc.ServerOption
+	grpcServer := grpc.NewServer(opts...)
+
+	pb.RegisterProductServiceServer(grpcServer, &controllers.ProductService{})
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	} 
 }
