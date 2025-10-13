f886edc7c69ab3a6689d0c07ae59a13269b97143
eddieogola <edwineogola@gmail.com>
2025-10-13 12:17:20 +0300

    feat: add product mutations to GraphQL resolvers for create, update, and delete operations

--- server/apigateway/src/resolvers/index.ts
+++ server/apigateway/src/resolvers/index.ts
@@ -1,10 +1,30 @@
-import { getProducts, getProductById } from "../controllers/productController";
+import {
+  getProducts,
+  getProductById,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+} from "../controllers/productController";
 import {} from "graphql";
+import { Product } from "../models/Product";
 
 const resolvers = {
   Query: {
     products: () => getProducts(),
     product: (_: any, args: { id: string }) => getProductById({ id: args.id }),
+  },
+  Mutation: {
+    createProduct: (_: any, args: { productData: Product }) =>
+      createProduct(args.productData),
+    updateProduct: (
+      _: any,
+      args: {
+        id: Pick<Product, "id">;
+        productData: Partial<Omit<Product, "id">>;
+      },
+    ) => updateProduct(args.id, args.productData),
+    deleteProduct: (_: any, args: { id: Pick<Product, "id"> }) =>
+      deleteProduct(args.id),
   },
 };
 
