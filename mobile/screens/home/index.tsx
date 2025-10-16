42646b63b1099cc80cdf950bc5f35937f02500cf
eddieogola <edwineogola@gmail.com>
2025-10-16 21:26:45 +0300

    refactor: update HomeScreen component to use View and StyleSheet for layout

--- mobile/screens/home/index.tsx
+++ mobile/screens/home/index.tsx
@@ -1,7 +1,19 @@
-import { Text } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 const HomeScreen = () => {
-  return <Text>Home Screen</Text>;
+  return (
+    <View style={styles.container}>
+      <Text>Home Screen</Text>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
 
 export default HomeScreen;
